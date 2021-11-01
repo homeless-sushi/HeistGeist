1bbf810473d26872a8eee27002be3d2a1d911b10
Guido Rolle <rolleguido@gmail.com>
2021-11-01 13:35:46 +0100

    add InputWrapper

---
+++ HeistGeist/Assets/Scripts/Player/InputWrapper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public readonly struct InputWrapper
+    {
+        public readonly Vector2 direction;
+        public readonly bool crouch;
+        public readonly bool interact;
+        
+        public InputWrapper(Vector2 direction, bool crouch, bool interact)
+        {
+            this.direction = direction;
+            this.crouch = crouch;
+            this.interact = interact;
+        }
+    }
+}
