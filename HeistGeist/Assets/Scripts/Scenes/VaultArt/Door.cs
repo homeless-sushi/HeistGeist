2695d02932d2c2c342c91c4f104459885625e446
Guido Rolle <rolleguido@gmail.com>
2021-11-18 14:16:19 +0100

    add Door

--- HeistGeist/Assets/Scripts/Scenes/VaultArt/Door.cs
+++ HeistGeist/Assets/Scripts/Scenes/VaultArt/Door.cs
@@ -1,9 +1,43 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Scenes.VaultArt
 {
     public class Door : MonoBehaviour
     {
+        [SerializeField] private Sprite openSprite;
+        //[SerializeField] private Sprite closedSprite;
+        private SpriteRenderer _doorSpriteRenderer;
+        [SerializeField] private Collider2D doorCollider;
+
+        //[SerializeField] private UnityEvent enterEvent;
+        //private bool open = false;
+
+        private void Awake()
+        {
+            _doorSpriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            //enterEvent.Invoke();
+            print("enter");
+        }
+
+        public void Open()
+        {
+            //open = true;
+            _doorSpriteRenderer.sprite = openSprite;
+            doorCollider.enabled = false;
+        }
+        /*
+        public void Close()
+        {
+            open = false;
+            _doorSpriteRenderer.sprite = openSprite;
+            doorCollider.enabled = true;
+        }
+        */
         [SerializeField] private int[] code;
 
         public void SetCode(int[] code)
--- HeistGeist/Assets/Scripts/TestScene.cs
+++ HeistGeist/Assets/Scripts/TestScene.cs
@@ -1,14 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using Scenes.VaultArt;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TestScene : MonoBehaviour
 {
     public int sceneNumber;
+    public Door door;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.N))
             SceneManager.LoadScene(sceneNumber);
+
+        if (Input.GetKeyDown(KeyCode.D))
+            door.Open();
     }
 }
