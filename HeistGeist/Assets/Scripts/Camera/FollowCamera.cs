abe5368d74b99e4136c82e14c33a71ea4c6c8889
Guido Rolle <rolleguido@gmail.com>
2021-12-08 14:56:00 +0100

    add BoundedFollowCamera

--- HeistGeist/Assets/Scripts/Camera/FollowCamera.cs
+++ HeistGeist/Assets/Scripts/Camera/FollowCamera.cs
@@ -4,9 +4,9 @@ namespace Camera
 {
     public class FollowCamera : MonoBehaviour
     {
-        [SerializeField] private Transform targetTransform; //Transform of the target to follow
-        [SerializeField] [Range(0f,1f)] private float smoothing = .5f; //Smoothing factor
-        private void FixedUpdate()
+        [SerializeField] protected Transform targetTransform; //Transform of the target to follow
+        [SerializeField] [Range(0f,1f)] protected float smoothing = .5f; //Smoothing factor
+        protected virtual void FixedUpdate()
         {
             Vector3 targetPosition = targetTransform.position;
             targetPosition.z = transform.position.z;
