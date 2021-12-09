b048e8ae9a8112a9de2cd105fc009edaed28af6b
Guido Rolle <rolleguido@gmail.com>
2021-12-09 10:23:39 +0100

    add Teleport and CameraBoundsData

---
+++ HeistGeist/Assets/Scripts/Camera/CameraBoundsData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Camera
+{
+    [CreateAssetMenu(menuName = "CameraBounds Data")]
+    public class CameraBoundsData : ScriptableObject
+    {
+        [SerializeField] private Vector2 cameraLowerBounds;
+        public Vector2 CameraLowerBounds => cameraLowerBounds;
+        [SerializeField] private Vector2 cameraUpperBounds;
+        public Vector2 CameraUpperBounds => cameraUpperBounds;
+    }
+}
