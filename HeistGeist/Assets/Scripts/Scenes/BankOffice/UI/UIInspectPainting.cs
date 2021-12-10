d113cec3fe813bd993cb695beaf3d1cb2dd7e56a
Guido Rolle <rolleguido@gmail.com>
2021-12-10 18:02:31 +0100

    add RotatingPaintingFrame prefab and UI for OfficeScene

---
+++ HeistGeist/Assets/Scripts/Scenes/BankOffice/UI/UIInspectPainting.cs
@@ -0,0 +1,64 @@
+﻿using Keypad.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scenes.BankOffice.UI
+{
+    public class UIInspectPainting : MonoBehaviour
+    {
+        [SerializeField] private Image paintingImage;
+        [SerializeField] private Image frameImage;
+        
+        [SerializeField] private KeypadButtonData rotateButtonData;
+        [SerializeField] private Button rotateButton;
+        public Button RotateButton => rotateButton;
+
+        [SerializeField] private KeypadButtonData enterButtonData;
+        [SerializeField] private Button enterButton;
+        public Button EnterButton => enterButton;
+        
+
+        private void Awake()
+        {
+            paintingImage.preserveAspect = true;
+            frameImage.preserveAspect = true;
+        }
+        
+        public void Display()
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void SetPaintingFrameAngle(Sprite painting, Sprite frame, Quaternion rotation)
+        {
+            paintingImage.sprite = painting;
+            paintingImage.transform.rotation = rotation;
+            frameImage.sprite = frame;
+            frameImage.transform.rotation = rotation;
+        }
+        
+        public void Solved()
+        {
+            rotateButton.enabled = false;
+            rotateButton.image.sprite = rotateButtonData.ButtonGreen;
+
+            enterButton.enabled = false;
+            enterButton.image.sprite = enterButtonData.ButtonGreen;
+        }
+        
+        public void Unsolved()
+        {
+            rotateButton.enabled = true;
+            rotateButton.image.sprite = rotateButtonData.ButtonGrey;
+
+            enterButton.enabled = true;
+            enterButton.image.sprite = enterButtonData.ButtonGrey;
+        }
+
+    }
+}
