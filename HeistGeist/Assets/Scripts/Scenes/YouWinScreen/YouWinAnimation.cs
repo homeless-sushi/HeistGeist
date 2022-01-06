d19cf8f70ada097a6c66d1697c31219780894e82
ZHANG_YD <yuedong.zhang@mail.polimi.it>
2022-01-06 17:21:53 +0100

    fix UI scaling and add Thank you

--- HeistGeist/Assets/Scripts/Scenes/YouWinScreen/YouWinAnimation.cs
+++ HeistGeist/Assets/Scripts/Scenes/YouWinScreen/YouWinAnimation.cs
@@ -5,21 +5,33 @@ namespace Scenes.YouWinScreen
     public class YouWinAnimation : MonoBehaviour
     {
         [SerializeField] private RectTransform scrollUpPanel;
+        [SerializeField] private GameObject thankYou;
         [SerializeField] private CanvasGroup restartMenu;
             
         void Start()
         {
             scrollUpPanel.gameObject.SetActive(true);
-            LeanTween.move(scrollUpPanel, new Vector3(0, -1100, 0), 0);
-            LeanTween.move(scrollUpPanel, new Vector3(0, 405, 0), 12f)
+            LeanTween.move(scrollUpPanel, new Vector3(0, -590, 0), 0);
+            LeanTween.move(scrollUpPanel, new Vector3(0, 390, 0), 15f)
                 .setEase(LeanTweenType.linear)
                 .setOnComplete(
                 ()=>
                 {
-                    restartMenu.gameObject.SetActive(true);
-                    LeanTween.alphaCanvas(restartMenu, 0f, 0);
-                    LeanTween.alphaCanvas(restartMenu, 1f, 4f)
-                        .setEase(LeanTweenType.easeInExpo);
+                    LeanTween.alphaCanvas(scrollUpPanel.gameObject.GetComponent<CanvasGroup>(), 1f, 0);
+                    LeanTween.alphaCanvas(scrollUpPanel.gameObject.GetComponent<CanvasGroup>(), 0f, 2f)
+                        .setEase(LeanTweenType.easeInExpo).setOnComplete(
+                            ()=>{
+                                thankYou.gameObject.SetActive(true);
+                                LeanTween.alphaCanvas(thankYou.GetComponent<CanvasGroup>(), 0f, 0);
+                                LeanTween.alphaCanvas(thankYou.GetComponent<CanvasGroup>(), 1f, 2f)
+                                    .setEase(LeanTweenType.easeInExpo).setOnComplete(
+                                        ()=>{
+                                            restartMenu.gameObject.SetActive(true);
+                                            LeanTween.alphaCanvas(restartMenu, 0f, 0);
+                                            LeanTween.alphaCanvas(restartMenu, 1f, 4f)
+                                                .setEase(LeanTweenType.easeInExpo);
+                                        });
+                            });
                 });
         }
     }
