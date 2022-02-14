a207671a378b91d5f4a54f716a47c8ce253ce32d
Guido Rolle <rolleguido@gmail.com>
2022-02-14 13:54:25 +0100

    fix Manhole UI

---
+++ HeistGeist/Assets/Scripts/Scenes/OutsideStreet/UI/Keyhole.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Scenes.OutsideStreet.UI
+{
+    public class Keyhole : MonoBehaviour
+    {
+        [SerializeField] private int correctKeyNumber;
+        public int CorrectKeyNumber => correctKeyNumber;
+
+        [SerializeField] private UnityEvent<bool> answer; 
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Key key = other.GetComponent<Key>();
+            if (key == null) return;
+
+            bool correctAnswer = key.KeyNumber == correctKeyNumber;
+            answer.Invoke(correctAnswer);
+        }
+    }
+}
